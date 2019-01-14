a9d103bb819bda2855ff7dc3c84679bde854f9ec
Dejan Andovski <Dejan.Andovski@sedc.mk>
2019-01-14 20:25:46 +0100

    'Add'

--- MyFirstProject/Scripts/script.js
+++ MyFirstProject/Scripts/script.js
@@ -1,5 +1,38 @@
 console.log('works')
-dadasdas
-dasdad
-asdasda
-sdasdad
+
+// function myFullName(name, nickname, lastname) {
+//     if(nickname === undefined) {
+//         return `${name} ${lastname}`;
+    
+// }
+
+// return `${name} ${nickname} ${lastname}`;
+
+// }
+
+console.log('Dejan', undefined, 'Andovski')
+
+
+const myFullname =  (name, nickname, lastname ) => `${name} ${nickname} ${lastname}`;
+
+
+// let sum = (a) => a = 0;
+
+const sumto =  num => {
+    let sum = 0;
+    for(let i = 0; i<= num; i++){
+        sum += i
+    }
+    return sum;
+}
+
+console.log(sumto(10));
+
+
+
+
+
+
+
+
+
