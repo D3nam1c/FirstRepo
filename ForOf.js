aee548026994a4a87b2d52bfd77d877a59714c67
D3nam1c <45211188+D3nam1c@users.noreply.github.com>
2019-01-16 20:58:09 +0100

    Add files via upload

---
+++ ForOf.js
@@ -0,0 +1,13 @@
+
+
+let animals = ['🐔', '🐷', '🐑', '🐇'];
+let names = ['Gertrude', 'Henry', 'Melvin', 'Billy Bob'];
+
+for (let animal of animals) {
+  // Random name for our animal
+  let nameIdx = Math.floor(Math.random() * names.length);
+
+  console.log(`${names[nameIdx]} the ${animal}`);
+}
+    
+//for of cicle
